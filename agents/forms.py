5efdb114eeb370c37f995b23b95c3bec65fc349b
SamirAlizada <samir.elizade5555@gmail.com>
2024-12-23 21:08:04 +0400

    - Introduced a new `ManagerForm` for the `Manager` model to handle form rendering and validation. - Included fields: `name`, `surname`, and the newly added `department`. - Ensured the existing `AgentForm` remains unchanged for managing `Agent` instances.

--- agents/forms.py
+++ agents/forms.py
@@ -1,8 +1,13 @@
 # agents/forms.py
 from django import forms
-from .models import Agent
+from .models import Agent, Manager
 
 class AgentForm(forms.ModelForm):
     class Meta:
         model = Agent
         fields = ['photo', 'name', 'surname', 'fin', 'status', 'group']
+
+class ManagerForm(forms.ModelForm):
+    class Meta:
+        model = Manager
+        fields = ['name', 'surname', 'department']
