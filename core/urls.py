5b519d8e0ce19a9aa932b051fffd591cff62ed06
SamirAlizada <samir.elizade5555@gmail.com>
2024-12-30 16:32:17 +0400

    Update URL routing to include authentication views
    - Added login and logout routes to the URL configuration.
    - Integrated custom login view from agents.views.
    - Configured LogoutView to redirect to the login page after logout.

--- core/urls.py
+++ core/urls.py
@@ -16,12 +16,16 @@ Including another URLconf
 # multi_user_project/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from agents.views import login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('agents.urls')),
+    path('login/', login_view, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
