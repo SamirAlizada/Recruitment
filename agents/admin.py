3ac82c97afd7283d82e26dd4f3f1cad07f5ae8a7
SamirAlizada <samir.elizade5555@gmail.com>
2025-01-12 23:53:44 +0400

    Add DepartmentHead model to admin registration
    - Registered the DepartmentHead model in the Django admin for better management.

--- agents/admin.py
+++ agents/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Agent, Group, Manager, Department
+from .models import Agent, Group, Manager, Department, DepartmentHead
 
 admin.site.register(Agent)
 admin.site.register(Group)
 admin.site.register(Manager)
 admin.site.register(Department)
+admin.site.register(DepartmentHead)
